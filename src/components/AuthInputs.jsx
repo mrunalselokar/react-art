76e29f8bce3c883d96324b8ddbdc5d1e66179c64
mrunalselokar <mrunal.selokar.20@gmail.com>
2023-12-11 20:18:12 +0530

    moved authinput styles to styled component

--- src/components/AuthInputs.jsx
+++ src/components/AuthInputs.jsx
@@ -1,31 +1,12 @@
 import { useState } from 'react';
-import { styled } from 'styled-components';
+import {
+	ControlContainer,
+	Input,
+	SolidButton,
+	TextButton,
+	Label,
+} from './auth-inputs.styles';
 
-const ControlContainer = styled.div`
-	display: flex;
-	flex-direction: column;
-	gap: 0.5rem;
-	margin-bottom: 1.5rem;
-`;
-const Label = styled.label`
-	display: block;
-	margin-bottom: 0.5rem;
-	font-size: 0.75rem;
-	font-weight: 700;
-	letter-spacing: 0.1em;
-	text-transform: uppercase;
-	color: ${({ invalid }) => (invalid ? '#f87171' : '#c1cbde ')};
-`;
-const Input = styled.input`
-	width: 100%;
-	padding: 0.75rem 1rem;
-	line-height: 1.5;
-	background-color: ${({ invalid }) => (invalid ? '#fed2d2' : '#fff ')};
-	color: ${({ invalid }) => (invalid ? '#ef4444' : '#374151 ')};
-	border: 1px solid ${({ invalid }) => (invalid ? '#f73f3f' : 'transparent')};
-	border-radius: 0.25rem;
-	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
-`;
 export default function AuthInputs() {
 	const [enteredEmail, setEnteredEmail] = useState('');
 	const [enteredPassword, setEnteredPassword] = useState('');
@@ -69,16 +50,16 @@ export default function AuthInputs() {
 				</p>
 			</ControlContainer>
 			<div className="actions">
-				<button
+				<TextButton
 					type="button"
 					className="text-button">
 					Create a new account
-				</button>
-				<button
+				</TextButton>
+				<SolidButton
 					className="button"
 					onClick={handleLogin}>
 					Sign In
-				</button>
+				</SolidButton>
 			</div>
 		</div>
 	);
---
+++ src/components/auth-inputs.styles.jsx
@@ -0,0 +1,49 @@
+import { styled } from 'styled-components';
+
+export const ControlContainer = styled.div`
+	display: flex;
+	flex-direction: column;
+	gap: 0.5rem;
+	margin-bottom: 1.5rem;
+`;
+export const Label = styled.label`
+	display: block;
+	margin-bottom: 0.5rem;
+	font-size: 0.75rem;
+	font-weight: 700;
+	letter-spacing: 0.1em;
+	text-transform: uppercase;
+	color: ${({ invalid }) => (invalid ? '#f87171' : '#c1cbde ')};
+`;
+export const Input = styled.input`
+	width: 100%;
+	padding: 0.75rem 1rem;
+	line-height: 1.5;
+	background-color: ${({ invalid }) => (invalid ? '#fed2d2' : '#fff ')};
+	color: ${({ invalid }) => (invalid ? '#ef4444' : '#374151 ')};
+	border: 1px solid ${({ invalid }) => (invalid ? '#f73f3f' : 'transparent')};
+	border-radius: 0.25rem;
+	box-shadow: 0 1px 3px 0 rgba(0, 0, 0, 0.1), 0 1px 2px 0 rgba(0, 0, 0, 0.06);
+`;
+
+export const SolidButton = styled.button`
+	padding: 1rem 2rem;
+	font-weight: 600;
+	text-transform: uppercase;
+	border-radius: 0.25rem;
+	color: white;
+	background-color: #7892ff;
+	border-radius: 6px;
+	border: none;
+	&:hover {
+		background-color: #0e4fd9;
+	}
+`;
+
+export const TextButton = styled.button`
+	color: white;
+	border: none;
+	&:hover {
+		color: #7892ff;
+	}
+`;
